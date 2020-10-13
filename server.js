bba6ac2bd46f96fa3fd1bbf181eeeca71a7adcca
topdev10 <k100cool@outlook.com>
2020-10-13 09:36:32 -0700

    update token

--- server.js
+++ server.js
@@ -17,11 +17,12 @@ async function sendMessage() {
         // 'dWokBPQOSj2DqehJlpyDG-:APA91bHnyRfZdZRjHNIKmVWEez_4tzHHW0auG47FvW_u6p8FNs25DHcTre2414OlrYVoaTgtkrnRM6vY51ks2NV_XdhinPL-8IGp4gNC1g29f0dSxN5xSRGdspgmWMQ2XcmZgsCpOVDd',
         'dlsyTMULT12xnYHWD0A7on:APA91bENEJS_fQMVJuqyAJ_eL5MHZN12rs6XJs-1rXV4rEOjYWnEoyHw9k3_q6kuWiPTpFFSbStlj0BTvR5EfZcs9BbldJTiMA7UILXVd1_-FP6OxVE-CDC3alLpKg4ilhFaJimlr1h5',
         'c27p5TqCRPShj8vmj2cJKt:APA91bFz9rWBpI8oqe7rzkgqwyrrKt-nracCV721phiOqJc4mWRnF6Ijl9dROadeAjbDb1bc1wkXRMIwpO3hWzVVCws_dLmKd6vm5BenFSUXh-AgQrteKb_B95n3Xu8OCSQqrCrbETLr',
-        'esxE1O5pQP-JkCKVQajyw6:APA91bGwrYZfPT0Mi1Nh5_cKL6z02czVUgAwrURNDltbqALlxqoFxcltpykvVdmylRCOt18baLt3aQyp1RO9D8eUa658aEmCMGBB9HF8q0VBhow1Sq5H0L0gQ0c0zEZ_AFGzfo1ELA-G'
+        'esxE1O5pQP-JkCKVQajyw6:APA91bGwrYZfPT0Mi1Nh5_cKL6z02czVUgAwrURNDltbqALlxqoFxcltpykvVdmylRCOt18baLt3aQyp1RO9D8eUa658aEmCMGBB9HF8q0VBhow1Sq5H0L0gQ0c0zEZ_AFGzfo1ELA-G',
+        'eqVM7UJ6a5eHacTPTzISqj:APA91bF2Vh494ItAd7yxHFWdTgmxVHLwVVM_1qJYexgHyWXR1xYh_xHYekmEIBCOzu_M4uGu-73YdZMDOjncmtnCwjJ32jWVn4CqLdjSdbih9mmxP_L49ehBmMHfq7s52BC2U8jaNWum'
       ],
       data: {
-        'picUrl': "https://www.thespaghettidetective.com/favicon-32x32.png",
-        // 'picUrl': 'https://www.thespaghettidetective.com/img/webcam.png',
+        // 'picUrl': "https://www.thespaghettidetective.com/favicon-32x32.png",
+        'picUrl': 'https://www.thespaghettidetective.com/img/webcam.png',
         // 'picUrl': "https://www.thespaghettidetective.com/img/gauge.png",
         'condensedTitle': `condensedTitle-${i}`,
         'longTitle': `longTitle-${i}`,
